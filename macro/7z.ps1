fe870bc2bb791e97f9892e5880928be1f5ea5565
tameval <114147355+tameval@users.noreply.github.com>
2023-04-17 20:30:51 +0900

    Update 7z.ps1

--- macro/7z.ps1
+++ macro/7z.ps1
@@ -1,3 +1,3 @@
-C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://github.com/crjtpp/tools_public/raw/main/7z2201-x64.exe $env:TEMP\7z.Exe
+C:\Windows\System32\Certutil.Exe -Urlcache -Split -F https://github.com/tameval/dev/raw/main/macro/7z2201-x64.exe $env:TEMP\7z.Exe
 Start-Process -FilePath $env:TEMP\7z.Exe -Args "/S" -Verb RunAs -Wait
 Remove-Item $env:TEMP\7z.Exe
